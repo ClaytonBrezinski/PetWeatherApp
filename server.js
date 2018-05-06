c6a02f9197e1d96cf21c84b8c913f277f13641da
Unknown <clayton.brez@gmail.com>
2018-05-05 21:19:08 -0600

    add react requirements

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const PORT = process.env.PORT || 3000
 const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.send("hello world"))
+
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/', (req, res) => res.send("hello world"))
   // will change this to: res.render('/dashboard'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))

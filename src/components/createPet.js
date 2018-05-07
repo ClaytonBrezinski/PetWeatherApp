875219973474d3f6a3c0befea1510025af8a7fd8
Unknown <clayton.brez@gmail.com>
2018-05-07 10:39:55 -0600

    create form for create pet page, have link back to main form
    commit per issue #5

--- src/components/createPet.js
+++ src/components/createPet.js
@@ -1,11 +1,106 @@
 import React from "react";
-import { Button } from "material-ui";
-var request = require('request');
+import { Button, TextField } from "material-ui";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default class CreatePetView extends React.Component {
+    state = {
+        name: "",
+        type: "",
+        breed: "",
+        location: "",
+        lat: "",
+        long: "",
+    };
+    handleChange(event) {
+        this.setState({ [event.target.id]: event.target.value });
+    }
+    formSubmit() {
+        axios({
+            method: "post",
+            url: "/createPet",
+            data: {
+                name: "test",
+                type: this.state.type,
+                breed: this.state.breed,
+                location: this.state.location,
+                lat: parseInt(this.state.lat),
+                long: parseInt(this.state.long),
+            },
+        })
+            .then(function(response) {
+                console.log(response);
+                //Perform action based on response
+            })
+            .catch(function(error) {
+                console.log(error);
+                //Perform action based on error
+            });
+        // return to main page
+    }
     render() {
-        return <div>
-            CreatePetView here
-            </div>;
+        return (
+            <div className="container">
+                <div className="row">
+                    <h2>Add pet to CoverYoDoggo</h2>
+                </div>
+                <div className="row">
+                    <form onSubmit={() => this.formSubmit()}>
+                        <TextField
+                            fullWidth
+                            id="name"
+                            label="Name"
+                            onChange={event => this.handleChange(event)}
+                            margin="normal"
+                        />
+                        <TextField
+                            fullWidth
+                            id="type"
+                            label="type"
+                            onChange={event => this.handleChange(event)}
+                            margin="normal"
+                        />
+                        <TextField
+                            fullWidth
+                            id="breed"
+                            label="breed"
+                            onChange={event => this.handleChange(event)}
+                            margin="normal"
+                        />
+                        <TextField
+                            fullWidth
+                            id="location"
+                            label="location"
+                            onChange={event => this.handleChange(event)}
+                            margin="normal"
+                        />
+                        <TextField
+                            fullWidth
+                            id="lat"
+                            label="lat"
+                            onChange={event => this.handleChange(event)}
+                            margin="normal"
+                        />
+                        <TextField
+                            fullWidth
+                            id="long"
+                            label="long"
+                            onChange={event => this.handleChange(event)}
+                            margin="normal"
+                        />
+                        <Link to={{ pathname: `/` }}>
+                            <Button style={{ backgroundColor: "#64d8cb" }} type="submit" variant="raised">
+                                Submit
+                            </Button>
+                        </Link>
+                        <Link to={{ pathname: `/` }}>
+                            <Button style={{ backgroundColor: "#ff94c2" }} variant="raised">
+                                Return
+                            </Button>
+                        </Link>
+                    </form>
+                </div>
+            </div>
+        );
     }
 }

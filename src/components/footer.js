8546d9d2642753e56bc353e04f2d2b4fa0383a18
Unknown <clayton.brez@gmail.com>
2018-05-07 11:17:50 -0600

    complete create pet page
    commit per issue #5

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import { Button } from "material-ui";
 export default class Footer extends React.Component {
     render() {
         return (
-            <footer className="page-footer" style={{ textAlign: "center", padding: "15px" }}>
+            <footer className="page-footer" style={{ textAlign: "center", padding: "15px", backgroundColor:"lightblue" }}>
                 <p className="text-muted font-weight-light" style={{ margin: "0" }}>
                     <a style={{ color: "white" }} href="https://github.com/OriginalMidas">
                         Contact: Clayton Brezinski
